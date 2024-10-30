7cef8238ff06f1b18471a12a3e824ca6d97f5382
Nova Slovenska zaveza <info@zaveza.si>
2024-10-30 12:44:27 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 # Use the Read the Docs theme
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Static files path (for custom CSS, etc.)
